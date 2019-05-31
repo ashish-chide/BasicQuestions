c2568c789fcbc0b03b7cefc81932886ca3de011d
ashish chide <ashishchide1101@gmail.com>
2019-06-01 01:34:33 +0530

    pie question added

--- BasicQuestions.js
+++ BasicQuestions.js
@@ -538,10 +538,51 @@
 // }
 
 
+// pie display
 
+// let a =[1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3,8,4,6,2,6,4,3,3,8,3,2,7,9,5,0,2,8,8,4,
+//         1,9,7,1,6,9,3,9,9,3,7,5,1,0,5,8,2,0,9,7,4,9,4,4,5,9,2,3,0,7,8,1,6,4,0,6,
+//         2,8,6,2,0,8,9,9,8,6,2,8,0,3,4,8,2,5,3,4,2,1,1,7,0,6,7,9,8,2,1,4,8,0,8,6,
+//         5,1,3,2,8,2,3,0,6,6,4,7,0,9,3,8,4,4,6,0,9,5,5,0,5,8,2,2,3,1,7,2,5,3,5,9,
+//         4,0,8,1,2,8,4,8,1, ,1,1,7,4,5,0,2,8,4,1,0,2,7,0]
+// let main = document.getElementById('ball');
 
+// a.forEach((el) => {
+//   let b = document.createElement("div");
+//   b.classList.add("ball");
+//   b.style.width = el*12 +"px";
+//   b.style.height = el*12 + "px";
+//   b.style.margin = el;
+//   let randomColor =  Math.floor(Math.random()*16777215).toString(16);
+//   b.style.background = "#" + randomColor;
+//   main.append(b);  
+// })
 
+// console.log(a.length)
 
+//css
+// .ball{
+//   height: 40px;
+//   border-radius:50%;
+//   position: relative;
+//   -webkit-animation: mymove 5s infinite; /* Safari 4.0 - 8.0 */
+//   animation: mymove 5s infinite;
+//   animation-direction: alternate;
+// }
+
+// @-webkit-keyframes mymove {
+//   from {left: 0px;}
+//   to {left: 200px;}
+// }
+
+// @keyframes mymove {
+//   from {left: 0px;}
+//   to {left: 200px;}
+// }
+
+// html 
+// <main id="ball">
+//   </main>
 
 
 
